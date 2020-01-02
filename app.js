ba3b5e416de52008272b35cbde8e12069f67a11a
mokeddemhalima <57575174+mokeddemhalima@users.noreply.github.com>
2020-01-02 15:04:00 +0100

    back_end_final*

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ var express = require('express');
  * @type {object} 
  */
 var app = express();
-app.use(function(req, res, next) {
-    res.setHeader("Content-Security-Policy", "script-src 'self' https://apis.google.com");
-    return next();
-});
 /**
  * Variable contenant le module retourné par require('./controleur').
  * @type {object} 
